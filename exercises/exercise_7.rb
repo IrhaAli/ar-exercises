bf024ee7799e0b7bf171100db27c209b19527eea
Irha Ali <irhaali_97@hotmail.ca>
2023-03-06 22:28:03 -0500

    Complete ex7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -8,5 +8,17 @@ require_relative './exercise_6'
 
 puts "Exercise 7"
 puts "----------"
+# 1. Add validations to two models to enforce the following business rules:
+# - BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a [custom validation method](http://guides.rubyonrails.org/active_record_validations.html#custom-methods) - **don't** use a `Validator` class)
 
-# Your code goes here ...
+# 2. Ask the user for a store name (store it in a variable)
+puts "Give me a store name"
+store_name = $stdin.gets.chomp
+
+# 3. Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+new_store = Store.create(name: store_name)
+new_employee = Employee.create(hourly_rate: 600)
+
+# 4. Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+puts new_store.errors.full_messages
+puts new_employee.errors.full_messages
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
+  validate :type_of_apparel
+
+  def type_of_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:apparel, "at least one type must be available")
+    end
+  end
 end
