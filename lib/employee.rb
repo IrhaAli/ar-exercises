f789d67fbd321c8f650f4d36308b9ecb360f0a20
Irha Ali <irhaali_97@hotmail.ca>
2023-03-07 00:11:27 -0500

    Implement AR Callbacks

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,5 +1,12 @@
+require 'securerandom'
+
 class Employee < ActiveRecord::Base
   belongs_to :store
   validates :first_name, :last_name, :store, presence: true
   validates :hourly_rate, numericality: { only_integer: true, greater_than: 40, less_than: 200 }
+
+  before_save do
+    string_length = 8
+    self.password = rand(36**string_length).to_s(36)
+  end
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,12 +1,19 @@
 class Store < ActiveRecord::Base
   has_many :employees
   validates :name, length: {minimum: 3}
-  validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than: -1 }
   validate :type_of_apparel
 
   def type_of_apparel
     if !mens_apparel && !womens_apparel
       errors.add(:apparel, "at least one type must be available")
+    end
+  end
+
+  before_destroy do
+    if Employee.where(store_id: self.id).count >= 1
+      self.errors.add(:base, "Can't be destroy because of it has one or more employees")
+      throw :abort
     end
   end
 end
